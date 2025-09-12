15f8d5efddaec4fd72a5f805bc4fe20d302eef4b
saraylov <saraylov@gmail.com>
2025-09-12 18:10:32 +0300

    web_app 0.0.3

---
+++ viteImproved.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte({
+      preprocess: []
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5176
+  },
+  build: {
+    outDir: 'distImproved'
+  },
+  base: './'
+})
